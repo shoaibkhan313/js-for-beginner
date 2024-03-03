8b1c6bb8ef4b4a5fbf5f20cf3a539086f61c0885
Muhammad Shoaib Khan (IoT Eng) <82607955+shoaibkhan313@users.noreply.github.com>
2024-03-03 18:46:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountName = "Shoaib"
+var villageNWA = "North Waziristan"
+accountId = "Miransha"
+
+console.log(accountId)
+console.table([accountId, accountName, villageNWA, accountId])
